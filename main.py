1e5bfc3355d1d27231bfc90d61f345e3dfeeff7c
kevim <k.v.n.such@gmail.com>
2023-01-05 19:28:21 -0300

    Arrumando bug

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import argparse
 import os.path as path
 import re
-
+from sys import exit
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
